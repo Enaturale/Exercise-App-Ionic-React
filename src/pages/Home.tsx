e4334469b4b1122b1b86ad2bbf56219c66734d17
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-11-26 14:14:34 +0100

    Creating my first ionic+react app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [present, dismiss] = useIonActionSheet();
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +19,18 @@ const Home: React.FC = () => {
         </IonHeader> */}
         {/* <ExploreContainer /> */}
         <IonButton>Click Me!</IonButton>
+
+        <IonButton
+          expand="block"
+          onClick={() =>
+            present({
+              buttons: [{ text: 'Ok' }, { text: 'Cancel' }],
+              header: 'Action Sheet'
+            })
+          }
+        >
+          Show ActionSheet
+        </IonButton>
         
       </IonContent>
     </IonPage>
