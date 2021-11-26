e072ae142717a6154efec0d4e5accf2bcdf8de22
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-11-26 12:56:51 +0100

    Creating my first ionic+react app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,16 +7,16 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Getting it Right!</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Getting it Right!</IonTitle>
           </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        </IonHeader> */}
+        {/* <ExploreContainer /> */}
       </IonContent>
     </IonPage>
   );
