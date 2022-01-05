120fb57297816be7836136599c8f127295425882
The Real Tech Lady <bukolaarowosebi@gmail.com>
2022-01-05 17:07:31 +0100

    Back to base

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,51 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonActionSheet } from '@ionic/react';
+import { IonButton, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonPage, IonText, IonTitle, IonToolbar, useIonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
 import './Home.css';
+import fit from "../images/monday.png";
 
 const Home: React.FC = () => {
   const [present, dismiss] = useIonActionSheet();
   return (
-    <IonPage>
-      <IonHeader>
+    <IonPage   >
+      {/* <IonHeader>
         <IonToolbar>
-          <IonTitle>Getting it Right!</IonTitle>
+          <IonTitle>Get Fit!</IonTitle>
         </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        {/* <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Getting it Right!</IonTitle>
-          </IonToolbar>
-        </IonHeader> */}
-        {/* <ExploreContainer /> */}
-        <IonButton>Click Me!</IonButton>
+      </IonHeader> */}
+      <IonContent fullscreen  >
 
-        <IonButton
-          expand="block"
-          onClick={() =>
-            present({
-              buttons: [{ text: 'Ok' }, { text: 'Cancel' }],
-              header: 'Action Sheet'
-            })
-          }
-        >
-          Show ActionSheet
+        {/* <IonText>
+        <h2 style={{color: "red", marginTop:'160px', 
+        fontSize:'30px', fontVariant:}}>
+          Get FIT!
+        </h2>
+        </IonText> */}
+
+        <div style={{ height: '560px'}}>
+        <div  style={{backgroundImage:`url(${fit})`,fontSize:'16px',height: '570px' }} >
+        <div style={{}}>
+          <IonText>
+            <h2 style={{color: "white", fontSize: '50px', marginLeft:'50px', 
+            paddingTop:'140px'}}>
+              Get Fit!
+            </h2>
+          </IonText>
+        </div>
+
+        <IonButton fill="solid" expand="block"  color="danger" onClick={() =>{}}
+        style={{fontSize:'16px', marginTop:'50px' }}>
+          Get Started
         </IonButton>
+        </div>
+        
+
+        </div>
+
+
+
+        
+      
         
       </IonContent>
     </IonPage>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,17 @@
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/react"
+
+const Home: React.FC = () => {
+    return(
+        <IonPage   >
+       {/* <IonHeader>
+        <IonToolbar>
+          <IonTitle>Get Fit!</IonTitle>
+        </IonToolbar>
+      </IonHeader>  */}
+      <IonContent fullscreen  >
+          <h2>Log In</h2>
+      </IonContent>
+      </IonPage>
+    )
+
+}
