47b24bcedd130e1aad390f04de9ffd6b8e69f613
The Real Tech Lady <bukolaarowosebi@gmail.com>
2022-01-12 12:31:11 +0100

    welcome page images

--- src/pages/welcomepage.tsx
+++ src/pages/welcomepage.tsx
@@ -1,5 +1,17 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonLabel, IonPage, IonText, IonTitle, IonToolbar, useIonActionSheet } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonPage, IonText, IonTitle, IonToolbar, useIonActionSheet } from '@ionic/react';
 import './welcomepage.css';
+import squat from '../images/squat.png';
+import skip from '../images/skiprope.jpg';
+import side from '../images/sidecrunch.jpg';
+import crunch from '../images/crunch.jpg';
+import donkey from '../images/donkey.png';
+import rope from '../images/rope.jpg';
+import cereal from '../images/cerealmeal.jpg';
+import rice from '../images/rice.jpg';
+import fruitsalad from '../images/fruitsalad.jpg';
+
+
+
 
 
 const Welcome: React.FC = () => {
@@ -9,7 +21,9 @@ const Welcome: React.FC = () => {
     <IonPage>
         <IonContent style={{overflow: 'hidden'}}>
             <IonTitle className="title">
-                <h2>Hi, Firstname. < IonIcon style={{marginLeft:'49px'}}name='menu-outline'/> </h2>
+                <h2>Hi, Firstname. 
+                  < IonIcon style={{marginLeft:'49px'}}name='menu-outline'/> 
+                </h2>
 
                 
             </IonTitle>
@@ -22,53 +36,70 @@ const Welcome: React.FC = () => {
 
             <IonCard>
               <IonCardHeader>
-                <IonCardTitle className='title'>WorkOuts</IonCardTitle>
+                <IonCardTitle className='title1'>WorkOuts  
+                <p className='seemore'>See All</p>                
+                </IonCardTitle>                
               </IonCardHeader>
               <IonCardContent>
-              <IonText ><p className="seemore">See more</p></IonText>
-                <IonCard>
-                  <IonCardHeader>
-                    <IonCardTitle>Squats</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>20 Squats per day</IonCardContent>
-                </IonCard>
 
-                <IonCard>
-                  <IonCardHeader>
-                    <IonCardTitle>Squats</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>20 Squats per day</IonCardContent>
-                </IonCard>
+                <IonItem>
+                  <IonLabel>
+                    <h4>Squats</h4>
+                    <p>20 Reps</p>
+                    <IonImg  src={squat}/>
+                  </IonLabel>
+                </IonItem>
 
-                <IonCard>
-                  <IonCardHeader>
-                    <IonCardTitle >Squats</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>20 Squats per day</IonCardContent>
-                </IonCard>
+                <IonItem>
+                <IonLabel>
+                    <h4>Donkey Kicks</h4>
+                    <p>20 Reps</p>
+                    <IonImg  src={donkey}/>
+                  </IonLabel>
+                </IonItem>  
+
+                <IonItem>
+                <IonLabel>
+                    <h4>Skip Rope</h4>
+                    <p>50 Jumps</p>
+                    <IonImg  src={rope}/>
+                  </IonLabel>
+                </IonItem>    
   
-                
               </IonCardContent>             
             </IonCard>
 
-            <IonCard>
+            {/* The second card begins here */}
+
+            <IonCard style={{marginTop: '20px'}}>
               <IonCardHeader>
-                <IonCardTitle className='title'>Diets and  Nuritions</IonCardTitle>
+                <IonCardTitle className='title1'>Diet Packs <p className='seemore'>See All</p>  </IonCardTitle>
               </IonCardHeader>
               <IonCardContent>
-              <IonCard>
-                  <IonCardHeader>
-                    <IonCardTitle>Squats</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>20 Squats per day</IonCardContent>
-                </IonCard>
+              <IonItem>
+                  <IonLabel>
+                    <h4>Breakfast</h4>
+                    <p>Fruit and Cereal Blend <br/> - 10 Receipes</p>
+                    <IonImg  src={cereal}/>
+                  </IonLabel>
+                </IonItem>
 
-                <IonCard>
-                  <IonCardHeader>
-                    <IonCardTitle>Squats</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>20 Squats per day</IonCardContent>
-                </IonCard>
+                <IonItem>
+                <IonLabel>
+                    <h4>Lunch</h4>
+                    <p>Rice/Pasta and Fish Sauce <br/>- 2 Receipes</p>
+                    <IonImg  src={rice}/>
+                  </IonLabel>
+                </IonItem>  
+
+                <IonItem>
+                <IonLabel>
+                    <h4>Dinner</h4>
+                    <p>Fruit Salad - 3 Receipes</p>
+                    <IonImg  src={fruitsalad}/>
+                  </IonLabel>
+                </IonItem>    
+  
 
               </IonCardContent>             
             </IonCard>
